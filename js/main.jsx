3ece9d6d56491d26d127ae4e4e190f4af29aa41e
vvargoal <vvargoal@gmail.com>
2019-02-11 15:05:13 -0500

    added requirements

--- js/main.jsx
+++ js/main.jsx
@@ -4,15 +4,15 @@ import LoginPage from './LoginPage';
 import ErrorBoundary from './ErrorBoundary';
 
 ReactDOM.render(
-  <ErrorBoundary>
+  // <ErrorBoundary>
     <LoginPage
       scope="user-read-email user-top-read user-read-recently-played playlist-modify-private"
       client_id="E62008c8210748e89b1baf8104e251ef"
       response_type="token"
       redirect_uri="http://localhost:1337/"
       spotify_authorize_uri="https://accounts.spotify.com/authorize"
-    />
-  </ErrorBoundary>,
+    />,
+  // </ErrorBoundary>,
 
   document.getElementById('reactEntry'),
 );
